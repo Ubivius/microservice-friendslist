9762a2cff1efbf0c0d704313bc71b013f6f3d4e4
Malcolm St. John <malcolm19@hotmail.com>
2021-11-05 14:56:54 -0400

    Updated interface to not have non exported methods

--- pkg/database/db_interface.go
+++ pkg/database/db_interface.go
@@ -13,10 +13,6 @@ type RelationshipDB interface {
 	UpdateRelationship(ctx context.Context, relationship *data.Relationship) error
 	AddRelationship(ctx context.Context, relationship *data.Relationship) error
 	DeleteRelationship(ctx context.Context, id string) error
-	validateRelationship(ctx context.Context, relationship *data.Relationship) error
-	relationshipExist(ctx context.Context, id string, userID1 string, userID2 string) (bool, error)
-	validateUserExist(ctx context.Context, userID string) bool
-	getConversationID(ctx context.Context, userID []string) (string, error)
 	Connect() error
 	PingDB() error
 	CloseDB()
--- pkg/database/mockDB.go
+++ pkg/database/mockDB.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"context"
 	"time"
 
 	"github.com/Ubivius/microservice-friendslist/pkg/data"
 	"github.com/google/uuid"
+	"go.opentelemetry.io/otel"
 )
 
 type MockRelationships struct {
@@ -27,7 +29,9 @@ func (mp *MockRelationships) CloseDB() {
 	log.Info("Mocked DB connection closed")
 }
 
-func (mp *MockRelationships) GetFriendsListByUserID(userID string) (*data.Relationships, error) {
+func (mp *MockRelationships) GetFriendsListByUserID(ctx context.Context, userID string) (*data.Relationships, error) {
+	_, span := otel.Tracer("friendslist").Start(ctx, "getFriendsListByUserIdDatabase")
+	defer span.End()
 	friendsList := findFriendsListByUserID(userID)
 	if len(friendsList) == 0 {
 		return nil, data.ErrorRelationshipNotFound
@@ -35,7 +39,9 @@ func (mp *MockRelationships) GetFriendsListByUserID(userID string) (*data.Relati
 	return &friendsList, nil
 }
 
-func (mp *MockRelationships) GetInvitesListByUserID(userID string) (*data.Relationships, error) {
+func (mp *MockRelationships) GetInvitesListByUserID(ctx context.Context, userID string) (*data.Relationships, error) {
+	_, span := otel.Tracer("friendslist").Start(ctx, "getFriendsRequestsByUserIdDatabase")
+	defer span.End()
 	invitesList := findInvitesListByUserID(userID)
 	if len(invitesList) == 0 {
 		return nil, data.ErrorRelationshipNotFound
@@ -43,7 +49,9 @@ func (mp *MockRelationships) GetInvitesListByUserID(userID string) (*data.Relati
 	return &invitesList, nil
 }
 
-func (mp *MockRelationships) UpdateRelationship(relationship *data.Relationship) error {
+func (mp *MockRelationships) UpdateRelationship(ctx context.Context, relationship *data.Relationship) error {
+	_, span := otel.Tracer("friendslist").Start(ctx, "updateRelationshipDatabase")
+	defer span.End()
 	index := findIndexByRelationshipID(relationship.ID)
 	if index == -1 {
 		return data.ErrorRelationshipNotFound
@@ -58,7 +66,9 @@ func (mp *MockRelationships) UpdateRelationship(relationship *data.Relationship)
 	return nil
 }
 
-func (mp *MockRelationships) AddRelationship(relationship *data.Relationship) error {
+func (mp *MockRelationships) AddRelationship(ctx context.Context, relationship *data.Relationship) error {
+	_, span := otel.Tracer("friendslist").Start(ctx, "addRelationshipDatabase")
+	defer span.End()
 	err := mp.validateRelationship(relationship)
 	if err == nil {
 		relationship.ID = uuid.NewString()
@@ -68,7 +78,9 @@ func (mp *MockRelationships) AddRelationship(relationship *data.Relationship) er
 	return err
 }
 
-func (mp *MockRelationships) DeleteRelationship(id string) error {
+func (mp *MockRelationships) DeleteRelationship(ctx context.Context, id string) error {
+	_, span := otel.Tracer("friendslist").Start(ctx, "deleteRelationshipDatabase")
+	defer span.End()
 	index := findIndexByRelationshipID(id)
 	if index == -1 {
 		return data.ErrorRelationshipNotFound
@@ -83,10 +95,10 @@ func (mp *MockRelationships) DeleteRelationship(id string) error {
 // Returns -1 when no relationship is found
 func findFriendsListByUserID(id string) data.Relationships {
 	var friendsList data.Relationships
-	for _ , relationship := range relationshipList {
+	for _, relationship := range relationshipList {
 		if relationship.User1.UserID == id && relationship.User1.RelationshipType == data.Friend {
 			friendsList = append(friendsList, relationship)
-		} else if relationship.User2.UserID == id && relationship.User2.RelationshipType == data.Friend{
+		} else if relationship.User2.UserID == id && relationship.User2.RelationshipType == data.Friend {
 			friendsList = append(friendsList, relationship)
 		}
 	}
@@ -97,10 +109,10 @@ func findFriendsListByUserID(id string) data.Relationships {
 // Returns -1 when no relationship is found
 func findInvitesListByUserID(id string) data.Relationships {
 	var invitesList data.Relationships
-	for _ , relationship := range relationshipList {
+	for _, relationship := range relationshipList {
 		if relationship.User1.UserID == id && relationship.User1.RelationshipType == data.PendingIncoming {
 			invitesList = append(invitesList, relationship)
-		} else if relationship.User2.UserID == id && relationship.User2.RelationshipType == data.PendingIncoming{
+		} else if relationship.User2.UserID == id && relationship.User2.RelationshipType == data.PendingIncoming {
 			invitesList = append(invitesList, relationship)
 		}
 	}
@@ -119,7 +131,7 @@ func findIndexByRelationshipID(id string) int {
 }
 
 func (mp *MockRelationships) validateRelationship(relationship *data.Relationship) error {
-	if !mp.validateUserExist(relationship.User1.UserID) || !mp.validateUserExist(relationship.User2.UserID){
+	if !mp.validateUserExist(relationship.User1.UserID) || !mp.validateUserExist(relationship.User2.UserID) {
 		return data.ErrorUserNotFound
 	}
 	if relationship.User1.UserID == relationship.User2.UserID {
@@ -142,10 +154,10 @@ func (mp *MockRelationships) validateUserExist(userID string) bool {
 
 // Returns an bool when a relationship with the two users is found
 func (mp *MockRelationships) relationshipExist(id string, userID1 string, userID2 string) (bool, error) {
-	for _ , relationship := range relationshipList {
+	for _, relationship := range relationshipList {
 		if relationship.ID != id &&
-		(relationship.User1.UserID == userID1 || relationship.User1.UserID == userID2) &&
-		(relationship.User2.UserID == userID1 || relationship.User2.UserID == userID2){
+			(relationship.User1.UserID == userID1 || relationship.User1.UserID == userID2) &&
+			(relationship.User2.UserID == userID1 || relationship.User2.UserID == userID2) {
 			return true, nil
 		}
 	}
@@ -162,35 +174,35 @@ func (mp *MockRelationships) getConversationID(userID []string) (string, error) 
 
 var relationshipList = []*data.Relationship{
 	{
-		ID:          	"a2181017-5c53-422b-b6bc-036b27c04fc8",
-		User1:        	data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.PendingOutgoing},
-		User2: 			data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.PendingIncoming},
-		ConversationID:	"a2181017-5c53-422b-b6bc-036b27c04fc8",
-		CreatedOn:   	time.Now().UTC().String(),
-		UpdatedOn:   	time.Now().UTC().String(),
+		ID:             "a2181017-5c53-422b-b6bc-036b27c04fc8",
+		User1:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.PendingOutgoing},
+		User2:          data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.PendingIncoming},
+		ConversationID: "a2181017-5c53-422b-b6bc-036b27c04fc8",
+		CreatedOn:      time.Now().UTC().String(),
+		UpdatedOn:      time.Now().UTC().String(),
 	},
 	{
-		ID:          	"e2382ea2-b5fa-4506-aa9d-d338aa52af44",
-		User1:        	data.User{UserID: "c5825d3e-8a77-11eb-8dcd-0242ac130003", RelationshipType: data.PendingOutgoing},
-		User2: 			data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.PendingIncoming},
-		ConversationID:	"e2382ea2-b5fa-4506-aa9d-d338aa52af44",
-		CreatedOn:   	time.Now().UTC().String(),
-		UpdatedOn:   	time.Now().UTC().String(),
+		ID:             "e2382ea2-b5fa-4506-aa9d-d338aa52af44",
+		User1:          data.User{UserID: "c5825d3e-8a77-11eb-8dcd-0242ac130003", RelationshipType: data.PendingOutgoing},
+		User2:          data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.PendingIncoming},
+		ConversationID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44",
+		CreatedOn:      time.Now().UTC().String(),
+		UpdatedOn:      time.Now().UTC().String(),
 	},
 	{
-		ID:          	"c5825d3e-8a77-11eb-8dcd-0242ac130003",
-		User1:        	data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
-		User2: 			data.User{UserID: "f171ea04-8a77-11eb-8dcd-0242ac130003", RelationshipType: data.Friend},
-		ConversationID:	"c5825d3e-8a77-11eb-8dcd-0242ac130003",
-		CreatedOn:   	time.Now().UTC().String(),
-		UpdatedOn:   	time.Now().UTC().String(),
+		ID:             "c5825d3e-8a77-11eb-8dcd-0242ac130003",
+		User1:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
+		User2:          data.User{UserID: "f171ea04-8a77-11eb-8dcd-0242ac130003", RelationshipType: data.Friend},
+		ConversationID: "c5825d3e-8a77-11eb-8dcd-0242ac130003",
+		CreatedOn:      time.Now().UTC().String(),
+		UpdatedOn:      time.Now().UTC().String(),
 	},
 	{
-		ID:          	"f171ea04-8a77-11eb-8dcd-0242ac130003",
-		User1:        	data.User{UserID: "0af831ea-8a78-11eb-8dcd-0242ac130003", RelationshipType: data.Friend},
-		User2: 			data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
-		ConversationID:	"f171ea04-8a77-11eb-8dcd-0242ac130003",
-		CreatedOn:   	time.Now().UTC().String(),
-		UpdatedOn:   	time.Now().UTC().String(),
+		ID:             "f171ea04-8a77-11eb-8dcd-0242ac130003",
+		User1:          data.User{UserID: "0af831ea-8a78-11eb-8dcd-0242ac130003", RelationshipType: data.Friend},
+		User2:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
+		ConversationID: "f171ea04-8a77-11eb-8dcd-0242ac130003",
+		CreatedOn:      time.Now().UTC().String(),
+		UpdatedOn:      time.Now().UTC().String(),
 	},
 }
