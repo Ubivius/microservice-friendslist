c175383e66284d6374398354d375d54735f1eea2
Malcolm St. John <malcolm19@hotmail.com>
2021-11-05 15:08:52 -0400

    handlers are done

--- pkg/handlers/delete.go
+++ pkg/handlers/delete.go
@@ -4,14 +4,17 @@ import (
 	"net/http"
 
 	"github.com/Ubivius/microservice-friendslist/pkg/data"
+	"go.opentelemetry.io/otel"
 )
 
 // Delete a relationship with specified id from the database
 func (relationshipHandler *RelationshipsHandler) Delete(responseWriter http.ResponseWriter, request *http.Request) {
+	_, span := otel.Tracer("friendslist").Start(request.Context(), "deleteRelationship")
+	defer span.End()
 	id := getRelationshipID(request)
 	log.Info("Delete relationship by ID request", "id", id)
 
-	err := relationshipHandler.db.DeleteRelationship(id)
+	err := relationshipHandler.db.DeleteRelationship(request.Context(), id)
 	switch err {
 	case nil:
 		responseWriter.WriteHeader(http.StatusNoContent)
