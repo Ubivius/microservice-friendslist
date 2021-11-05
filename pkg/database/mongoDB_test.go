165e1967b7fa0ed0a66f76945b6715b49b7b4b9f
Malcolm St. John <malcolm19@hotmail.com>
2021-11-05 15:03:42 -0400

    Fixed context for mongoDB tests

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"testing"
 
 	"github.com/Ubivius/microservice-friendslist/pkg/data"
@@ -24,13 +25,13 @@ func TestMongoDBAddRelationshipIntegration(t *testing.T) {
 	}
 
 	relationship := &data.Relationship{
-		User1: 			data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.PendingOutgoing},
-		User2:       	data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af45", RelationshipType: data.PendingIncoming},
+		User1:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.PendingOutgoing},
+		User2:          data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af45", RelationshipType: data.PendingIncoming},
 		ConversationID: "",
 	}
 
 	mp := NewMongoRelationships()
-	err := mp.AddRelationship(relationship)
+	err := mp.AddRelationship(context.Background(), relationship)
 	if err != nil {
 		t.Errorf("Failed to add relationship to database")
 	}
@@ -43,14 +44,14 @@ func TestMongoDBUpdateRelationshipIntegration(t *testing.T) {
 	}
 
 	relationship := &data.Relationship{
-		ID:          	"eb9aff9f-8c4e-47c3-9f6d-bd9aac3d9f31",
-		User1: 			data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
-		User2:       	data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.Friend},
+		ID:             "eb9aff9f-8c4e-47c3-9f6d-bd9aac3d9f31",
+		User1:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.Friend},
+		User2:          data.User{UserID: "e2382ea2-b5fa-4506-aa9d-d338aa52af44", RelationshipType: data.Friend},
 		ConversationID: "",
 	}
 
 	mp := NewMongoRelationships()
-	err := mp.UpdateRelationship(relationship)
+	err := mp.UpdateRelationship(context.Background(), relationship)
 	if err != nil {
 		t.Fail()
 	}
@@ -63,7 +64,7 @@ func TestMongoDBGetFriendsListByUserIDIntegration(t *testing.T) {
 	}
 
 	mp := NewMongoRelationships()
-	_, err := mp.GetFriendsListByUserID("a2181017-5c53-422b-b6bc-036b27c04fc8")
+	_, err := mp.GetFriendsListByUserID(context.Background(), "a2181017-5c53-422b-b6bc-036b27c04fc8")
 	if err != nil {
 		t.Fail()
 	}
@@ -77,11 +78,10 @@ func TestMongoDBGetInvitesListByUserIDIntegration(t *testing.T) {
 	}
 
 	mp := NewMongoRelationships()
-	_, err := mp.GetInvitesListByUserID("e2382ea2-b5fa-4506-aa9d-d338aa52af84")
+	_, err := mp.GetInvitesListByUserID(context.Background(), "e2382ea2-b5fa-4506-aa9d-d338aa52af84")
 	if err != nil {
 		t.Fail()
 	}
 
 	mp.CloseDB()
 }
-
