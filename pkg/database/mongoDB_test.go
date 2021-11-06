9cf9d82334e7b1236dd9bddaf8660f3933c9d9e0
Malcolm St. John <malcolm19@hotmail.com>
2021-11-06 16:30:15 -0400

    setup and teardown working

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -2,15 +2,39 @@ package database
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/Ubivius/microservice-friendslist/pkg/data"
 )
 
+func integrationTestSetup(t *testing.T) {
+	t.Log("Test setup")
+
+	if os.Getenv("DB_USERNAME") == "" {
+		os.Setenv("DB_USERNAME", "admin")
+	}
+	if os.Getenv("DB_PASSWORD") == "" {
+		os.Setenv("DB_PASSWORD", "pass")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		os.Setenv("DB_PORT", "27888")
+	}
+	if os.Getenv("DB_HOSTNAME") == "" {
+		os.Setenv("DB_HOSTNAME", "localhost")
+	}
+
+	err := deleteAllRelationshipsFromMongoDB()
+	if err != nil {
+		t.Errorf("Failed to delete existing relationships from database during setup")
+	}
+}
+
 func TestMongoDBConnectionAndShutdownIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoRelationships()
 	if mp == nil {
@@ -23,6 +47,7 @@ func TestMongoDBAddRelationshipIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	relationship := &data.Relationship{
 		User1:          data.User{UserID: "a2181017-5c53-422b-b6bc-036b27c04fc8", RelationshipType: data.PendingOutgoing},
@@ -42,6 +67,7 @@ func TestMongoDBUpdateRelationshipIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	relationship := &data.Relationship{
 		ID:             "eb9aff9f-8c4e-47c3-9f6d-bd9aac3d9f31",
@@ -62,6 +88,7 @@ func TestMongoDBGetFriendsListByUserIDIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoRelationships()
 	_, err := mp.GetFriendsListByUserID(context.Background(), "a2181017-5c53-422b-b6bc-036b27c04fc8")
@@ -76,6 +103,7 @@ func TestMongoDBGetInvitesListByUserIDIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoRelationships()
 	_, err := mp.GetInvitesListByUserID(context.Background(), "e2382ea2-b5fa-4506-aa9d-d338aa52af84")
