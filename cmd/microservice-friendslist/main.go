733a0635989986bfedc0b4ad1f640531e2e11bd0
Malcolm St. John <malcolm19@hotmail.com>
2021-11-23 15:55:15 -0500

    jaeger os endpoint

--- cmd/microservice-friendslist/main.go
+++ cmd/microservice-friendslist/main.go
@@ -27,7 +27,7 @@ func main() {
 	logf.SetLogger(newLogger.WithName("log"))
 
 	// Starting tracer provider
-	tp := tracing.CreateTracerProvider("http://192.168.6.12:14268/api/traces", "microservice-friendslist-traces")
+	tp := tracing.CreateTracerProvider(os.Getenv("JAEGER_ENDPOINT"), "microservice-friendslist-traces")
 
 	// Starting metrics exporter
 	metrics.StartPrometheusExporterWithName("friendslist")
